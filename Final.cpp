cf3bf4f52721c6f008e9982e37c0d7680ffd8c03
santiagolopezdev <97929350+santiagolopezdev@users.noreply.github.com>
2022-10-12 21:32:56 -0300

    Cambios A TODO DINAMICO
    Alo

--- Final.cpp
+++ Final.cpp
@@ -3,124 +3,142 @@
 
 using namespace std;
 
-struct dato {
-       char nombre[30];
-       int n;
-       int otro;
-       int v[50][50];
-       
-       };
-       
+struct dato
+{
+  char nombre[30];
+  int n;
+  int otro;
+  int v[50][50];
+};
 
-int Abrir (struct dato *d);
-void Valor (struct dato *d);
-int Guardar (struct dato *d);
+int Abrir(struct dato *d);
+void Valor(struct dato *d);
+int Guardar(struct dato *d);
 
+int main(void)
+{
 
-int main (void){
-    
-dato d;
-int error=0;
-d.v[0][0]=0;
+  dato d;
+  int error = 0;
+  d.v[0][0] = 0;
 
-error=Abrir (&d); //funcion
-if (error==1){
- cout << "\nNo se pudo abrir el archivo";
- return 0;}/*
+  error = Abrir(&d); //funcion
+  if (error == 1)
+  {
+    cout << "\nNo se pudo abrir el archivo";
+    return 0;
+  } /*
 error=Comprirmir (&d);
 if (error==2){
               cout << "\nFallo la descomprecion";
               return 0;
               }*/
 
-Valor (&d);
-error = Guardar (&d);
-if (error==3){
-              cout << "\nFallo la descomprecion";
-              return 0;
-              }
+  Valor(&d);
+  error = Guardar(&d);
+  if (error == 3)
+  {
+    cout << "\nFallo la descomprecion";
+    return 0;
+  }
 
-return 0;
+  return 0;
 }
 
+int Abrir(struct dato *d)
+{
 
-int Abrir (struct dato *d){
+  int i, v[1000], s = 0, n = 0, j;
+  ifstream archivo;
 
-int i, v[1000],s=0,n=0,j;
-ifstream archivo;    
-    
-cout << endl << "Ingrese el nombre de archivo ";
-cin >> d->nombre;
-archivo.open(d->nombre);
+  cout << endl
+       << "Ingrese el nombre de archivo ";
+  cin >> d->nombre;
+  archivo.open(d->nombre);
 
-while (!archivo.eof()){
-for (i=0;i<=9999;i++){
+  while (!archivo.eof())
+  {
+    for (i = 0; i <= 9999; i++)
+    {
 
-   archivo >> v[i];
-  // if(!archivo.eof());
-//n++;
-}
-}
-d->n=n/50;
-
-
-
-s=v[0];
-
-for (j=0;j<=d->n;j++){
-    d->v[j][0]=v[j*50];
-for (i=0;i<=48;i++){
-    d->v[j][i+1]=d->v[j][i]+v[i+1];
-    
+      archivo >> v[i];
+      // if(!archivo.eof());
+      //n++;
     }
-//d->v[j][50]=v[j][50];/*
-for (j=0;j<=d->n;j++){
-for (i=50;i<=49;i++){
-    d->v[j][i+1]=d->v[j][i]+v[i+1];
+  }
+  d->n = n / 50;
+
+  s = v[0];
+
+  for (j = 0; j <= d->n; j++)
+  {
+    d->v[j][0] = v[j * 50];
+    for (i = 0; i <= 48; i++)
+    {
+      d->v[j][i + 1] = d->v[j][i] + v[i + 1];
     }
-}}}
-void Valor (struct dato *d){
+    //d->v[j][50]=v[j][50];/*
+    for (j = 0; j <= d->n; j++)
+    {
+      for (i = 50; i <= 49; i++)
+      {
+        d->v[j][i + 1] = d->v[j][i] + v[i + 1];
+      }
+    }
+  }
 
-int i,j,m;
-int e;
+  return 0;
+}
+void Valor(struct dato *d)
+{
 
-float s=0;
-for (j=0;j<=d->n;j++){
-m=d->v[j][0];
-e=d->v[j][0];
-for (i=0;i<=49;i++){
-    
-s=d->v[j][i]+s;
+  int i, j, m;
+  int e;
+
+  float s = 0;
+  for (j = 0; j <= d->n; j++)
+  {
+    m = d->v[j][0];
+    e = d->v[j][0];
+    for (i = 0; i <= 49; i++)
+    {
+
+      s = d->v[j][i] + s;
+    }
+
+    for (i = 1; i <= 49; i++)
+    {
+      if (m > d->v[j][i])
+      {
+        m = d->v[j][i];
+      }
+      if (e < d->v[j][i])
+      {
+        e = d->v[j][i];
+      }
+    }
+  }
+  cout << s << "/100= " << s / 100 << " || " << m << "  " << e;
 }
 
-for (i=1;i<=49;i++){
-    if (m >d->v[j][i]){
-       m=d->v[j][i];
-       }
-     if (e<d->v[j][i]){
-       e=d->v[j][i];}
-}
-}
-cout << s << "/100= " << s/100 << " || " << m << "  " << e; 
-}
+int Guardar(struct dato *d)
+{
 
+  int i, j;
+  char nombre[30];
 
-int Guardar (struct dato *d){
-    
-    int i,j;
-    char nombre[30];
-    
-    
-    ofstream guarda;
-      cout << "\nComo guardar el archivo "; 
-    cin >> nombre;
-    guarda.open(nombre);
-  for (j=0;j<=(d->n+1);j++){
-    for (i=0;i<=49;i++){
-        guarda << d->v[j][i] << " ";
-        
-        } 
-        guarda << "\n";}
-    guarda.close();
-    return 0;    
+  ofstream guarda;
+  cout << "\nComo guardar el archivo ";
+  cin >> nombre;
+  guarda.open(nombre);
+  for (j = 0; j <= (d->n + 1); j++)
+  {
+    for (i = 0; i <= 49; i++)
+    {
+      guarda << d->v[j][i] << " ";
+    }
+    guarda << "\n";
+  }
+  guarda.close();
+  return 0;
 }
--- Modificado.cpp
+++ Modificado.cpp
@@ -1,153 +1,164 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
 struct dato {
-       char nombre[30];
-       vector<vector<int>> matriz;
-       int min;
-       int max;
-       float promedio;       
+  char nombre[30];
+  vector<vector<int>> matriz;
+  int min;
+  int max;
+  float promedio;
+  int cantMuestras = 0;
 };
-       
-const int MAX_FILA=50;
 
-int AbrirArchivo (struct dato *d);
+int AbrirArchivo(struct dato *d);
 int Decodificar(struct dato *d);
-int ValorMaxMinProm (struct dato *d);
-int GuardarArchivo (struct dato *d);
+int ValorMaxMinProm(struct dato *d);
+int GuardarArchivo(struct dato *d);
 void ImprimirPorConsola(struct dato *d);
+void LiberarMemoria(struct dato *d);
 
-int main (void){
+int main(void) {
+  dato* d = new struct dato;
+  int error = 0;
 
-  dato d;
-  int error=0;
-
-  error=AbrirArchivo(&d); //Funcion que carga los datos del archivo
-  if (error==1){
-   cout << "\nNo se pudo abrir el archivo \n";
-   return 0;
+  error = AbrirArchivo(d); // Funcion que carga los datos del archivo
+  if (error == 1) {
+    cout << "\nNo se pudo abrir el archivo \n";
+    return 0;
   }
 
-  error=Decodificar(&d); //Funcion que comprime los datos del archivo
-  if (error==1){
+  error = Decodificar(d); // Funcion que comprime los datos del archivo
+  if (error == 1) {
     cout << "\nFallo comprimiendo el texto";
     return 0;
   }
-
-  //ImprimirPorConsola(&d);
-
-  error = ValorMaxMinProm (&d);
-  if(error==1){
-    cout <<"\nFallo obteniendo el maximo/min/promedio";
+  // ImprimirPorConsola(&d);
+  error = ValorMaxMinProm(d);
+  if (error == 1) {
+    cout << "\nFallo obteniendo el maximo/min/promedio";
     return 0;
   }
-  error = GuardarArchivo(&d);
-  if (error==1){
+  error = GuardarArchivo(d);
+  if (error == 1) {
     cout << "\nFallo guardando el archivo";
     return 0;
   }
-
+  delete d;
   return 0;
 }
 
-
-int AbrirArchivo(struct dato *d){
+int AbrirArchivo(struct dato *d) {
   ifstream archivo;
-
   cout << endl << "Ingrese el nombre de archivo: \n";
   cin >> d->nombre;
-  
+
   archivo.open(d->nombre);
-  if(!archivo){
+  if (!archivo) {
     return 1;
   }
-  string palabra;
-  int contadorInterno = 0;
-  vector<int>* temp = new vector<int>;
-
-  while (archivo >> palabra){
-    contadorInterno++;
-    int num = stoi(palabra);
-    if(contadorInterno<=50){
+  string *palabra = new string;
+  int *contadorInterno = new int;
+  *contadorInterno = 0;
+  vector<int> *temp = new vector<int>;
+  while (archivo >> *palabra) {
+    *contadorInterno = *contadorInterno + 1;
+    d->cantMuestras++;
+    int num = stoi(*palabra);
+    if (*contadorInterno <= 50) {
       temp->push_back(num);
-    }
-    else{
-      contadorInterno = 1;
+
+    } else {
+      *contadorInterno = 1;
       d->matriz.push_back(*temp);
       temp = new vector<int>;
-      temp->push_back(num); 
+      temp->push_back(num);
     }
   }
-  if(temp->size()!=0){
+  if (temp->size() != 0) {
     d->matriz.push_back(*temp);
   }
+  delete palabra;
+  delete contadorInterno;
+  delete temp;
+  archivo.close();
   return 0;
 }
 
-int ValorMaxMinProm(struct dato *d){
-  int cantElem = 0;
+int ValorMaxMinProm(struct dato *d) {
   d->max = d->matriz[0][0];
   d->min = d->matriz[0][0];
   d->promedio = 0;
+  int* i = new int;
+  int* j = new int;
   
-  for (int i=0; i<d->matriz.size(); i++){
-    for (int j=0; j<d->matriz[i].size(); j++){
-      if (d->max < d->matriz[i][j]){
-        d->max = d->matriz[i][j];
+  for (*i = 0; *i < d->matriz.size(); *i = *i+1) {
+    for (*j = 0; *j < d->matriz[*i].size(); *j = *j+1) {
+      if (d->max < d->matriz[*i][*j]) {
+        d->max = d->matriz[*i][*j];
       }
-      if (d->min > d->matriz[i][j]){
-        d->min = d->matriz[i][j];
+      if (d->min > d->matriz[*i][*j]) {
+        d->min = d->matriz[*i][*j];
       }
-      d->promedio = d->promedio + d->matriz[i][j];
-      cantElem++;
+      d->promedio = d->promedio + d->matriz[*i][*j];
     }
   }
-   cout<<"La matriz tiene " << cantElem <<" Elementos"<<endl;
-  d->promedio = d->promedio/cantElem;
+  cout << "La matriz tiene " << d->cantMuestras << " Elementos" << endl;
+  d->promedio = d->promedio / d->cantMuestras;
   cout << "Promedio: " << d->promedio << " || Maximo:" << d->max << " || Minimo:" << d->min;
-
-  return 0;  
+  delete i;
+  delete j;
+  return 0;
 }
 
-int GuardarArchivo(struct dato *d){  
-  char nombre[30];
+int GuardarArchivo(struct dato *d) {
   ofstream guarda;
-  cout << "\nComo guardar el archivo "; 
-  cin >> nombre;
-  guarda.open(nombre);
-  for (int i=0; i<d->matriz.size(); i++){
-    for (int j=0; j<d->matriz[i].size(); j++){
-      guarda << d->matriz[i][j] << " ";
-    } 
+  cout << "\nIngrese nombre del archivo a guardar \n";
+  cin >> d->nombre;
+  guarda.open(d->nombre);
+  guarda << "Cantidad de cuadros almacenados: " << d->matriz.size() << " - "<< d->cantMuestras << " muestras contenidas en total\n";
+  int* i = new int;
+  int* j = new int;
+  for (*i = 0; *i < d->matriz.size(); *i = *i+1) {
+    for (*j = 0; *j < d->matriz[*i].size(); *j = *j+1) {
+      guarda << d->matriz[*i][*j] << " ";
+    }
     guarda << "\n";
   }
   guarda.close();
+  delete i;
+  delete j;
   
-  return 0;    
+  return 0;
 }
 
-void ImprimirPorConsola(struct dato *d){
-  cout<<"La matriz tiene " << d->matriz.size()<<" filas"<<endl;
-  for (int i = 0; i < d->matriz.size(); i++){
-    for (int j = 0; j < d->matriz[i].size(); j++){
-        cout << d->matriz[i][j]<<" ";
+void ImprimirPorConsola(struct dato *d) {
+  int* i = new int;
+  int* j = new int;
+  for (*i = 0; *i < d->matriz.size(); *i = *i+1) {
+    for (*j = 0; *j < d->matriz[*i].size(); *j = *j+1) {
+      cout << d->matriz[*i][*j] << " ";
     }
     cout << endl;
   }
+
+  delete i;
+  delete j;
 }
 
-int Decodificar(struct dato *d){
-  vector< vector<int> > decodificado;
-  
-  for (int i = 0; i < d->matriz.size(); i++){
-    int valorPrevio= d->matriz[i][0];
-    for (int j = 1; j < d->matriz[i].size(); j++){
-      d->matriz[i][j] = valorPrevio + d->matriz[i][j];
-      valorPrevio = d->matriz[i][j];
+int Decodificar(struct dato *d) {
+  int* i = new int;
+  int* j = new int;
+  for (*i = 0; *i < d->matriz.size(); *i = *i+1) {
+    int valorPrevio = d->matriz[*i][0];
+    for (*j = 1; *j < d->matriz[*i].size(); *j = *j+1) {
+      d->matriz[*i][*j] = valorPrevio + d->matriz[*i][*j];
+      valorPrevio = d->matriz[*i][*j];
     }
   }
+  delete i;
+  delete j;
   return 0;
 }
